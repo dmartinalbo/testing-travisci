0be7ce79e7acb1cbc8b3e48e4c0a7c9064efd1ad
Daniel Martín-Albo <dmas@wiris.com>
2020-05-15 15:13:29 +0200

    More changes

---
+++ .travis/push.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout master
+  echo "testing" >> foo.txt
+  git add foo.txt
+  git commit -m "Travis did this. Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/dmartinalbo/testing-travisci.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master 
+}
+
+setup_git
+commit_website_files
+upload_files
+
