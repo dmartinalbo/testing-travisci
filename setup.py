4d74e7539ff99d8aa13d808b789ce8f6a7fb26e6
Daniel Martín-Albo <dmas@wiris.com>
2020-05-15 12:37:21 +0200

    Skeleton ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='funniest',
+  version='0.1',
+  description='The funniest joke in the world',
+  author='dmass',
+  license='MIT',
+  packages=['funniest'],
+  zip_safe=False
+)
