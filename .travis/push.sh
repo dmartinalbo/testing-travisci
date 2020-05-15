070d78b115c26bd8ab0c573cb9cfb9751300e24f
Daniel Martín-Albo <dmas@wiris.com>
2020-05-15 15:33:18 +0200

    More more changes

--- .travis/push.sh
+++ .travis/push.sh
@@ -13,8 +13,9 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/dmartinalbo/testing-travisci.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master 
+  git remote rm origin
+  git remote add https://dmartinalbo@${GH_TOKEN}@github.com/dmartinalbo/testing-travisci.git > /dev/null 2>&1
+  git push --quiet origin master 
 }
 
 setup_git
