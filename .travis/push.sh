cb880e1d6dffe949f80fb439cbf587975a578474
Daniel Martín-Albo <dmas@wiris.com>
2020-05-17 17:19:48 +0200

    Improved CD

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,20 +3,27 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  
+  git remote rm origin
+  git remote add origin https://dmartinalbo:${GH_TOKEN}@github.com/dmartinalbo/testing-travisci.git > /dev/null 2>&1
 }
 
-commit_website_files() {
+bump_version() {
   git checkout master
   bump2version minor --allow-dirty --verbose --tag-name 'v{new_version}' --message "[skip ci] Bump to version {new_version}"
 }
 
-upload_files() {
-  git remote rm origin
-  git remote add origin https://dmartinalbo:${GH_TOKEN}@github.com/dmartinalbo/testing-travisci.git > /dev/null 2>&1
-  git push --quiet origin master --tags 
+merge() {
+  git checkout -B stable
+  git merge --no-ff -m "[skip ci] merging 'master' into 'stable'" stable 
+}
+
+push_changes() {
+  git push --quiet origin master stable --tags 
 }
 
 setup_git
-commit_website_files
-upload_files
+bump_version
+merge
+push_changes
 
